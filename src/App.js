5663e1c4403312fce4f92ee819eeab31352d22ca
meustar <meustar829@gmail.com>
2024-12-04 04:05:11 +0900

    react-router-dom 버전이슈에 따른 코드수정 component -> element 및 함수형 컴포넌트로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
-import "./App.css";
+import About from "./routes/About";
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,55 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Movie from "../components/Movie";
 import "./Home.css";
 
-class Home extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
+function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const {
+        data: {
+          data: { movies },
+        },
+      } = await axios.get(
+        "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+      );
+      setMovies(movies);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Failed to fetch movies:", error);
+    }
   };
 
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
+  useEffect(() => {
+    getMovies();
+  }, []); // 빈 배열은 컴포넌트가 마운트될 때 한 번만 실행됨
 
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Home;
